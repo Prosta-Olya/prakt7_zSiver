c913226923927c9f386a13e5f313cc0b72b5028e
Prosta-Olya <siverolya@gmail.com>
2025-02-11 15:53:01 +0500

    Add files via upload

--- zd7_2.kt
+++ zd7_2.kt
@@ -6,9 +6,14 @@ fun main(){
         val y = readLine()!!.toDouble()
         println("Введите z:")
         val z = readLine()!!.toDouble()
-        val a = (3+Math.pow(Math.E,y-1))/(1+Math.pow(x,2.0)*Math.abs(y-Math.tan(z)))
-        val b = 1+Math.abs(y-x)+Math.pow(y-x,2.0)/2+Math.pow(Math.abs(y-x),3.0)/3
-        println("Значение a = $a, значение b = $b")
+        when {
+            x==0.0 || y==0.0 || z==0.0 -> println("Значения не могут быть нулевыми, тк на ноль делить нельзя!")
+            else -> {
+                val a = (3 + Math.pow(Math.E, y - 1)) / (1 + Math.pow(x, 2.0) * Math.abs(y - Math.tan(z)))
+                val b = 1 + Math.abs(y - x) + Math.pow(y - x, 2.0) / 2 + Math.pow(Math.abs(y - x), 3.0) / 3
+                println("Значение a = $a, значение b = $b")
+            }
+        }
     }
     catch(e:Exception)
     {
